cce9613bef61566521ee463a19aa008d6b4be125
Muzzammil Alam <muza@ciklum.com>
2024-09-26 10:16:14 +0500

    eslint and pritier added

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,19 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules:{
+       "react/jsx-uses-react": "off",
+       "react/react-in-jsx-scope": "off",
+    }
+  }
 ];
